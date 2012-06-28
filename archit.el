3f8ea92ed9618876bec89c01a04ba03daf6e1b50
Archit Baweja <architbaweja@gmail.com>
2012-06-28 07:11:15 -0700

    Added some customizations.

--- archit.el
+++ archit.el
@@ -7,3 +7,17 @@
 
 (add-hook 'coffee-mode-hook
           '(lambda () (set (make-local-variable 'tab-width) 2)))
+
+(global-set-key (kbd "M-t") 'ack-and-a-half)
+
+; Override default find-file to find-file-in-other-window
+(global-set-key (kbd "C-x C-f") 'find-file-other-window)
+; Override default kill-buffer binding
+(global-set-key (kbd "C-x k") 'kill-buffer)
+
+; Find-file-in-project customizations
+(require 'find-file-in-project)
+(global-set-key (kbd "C-x 4 f") 'find-file-in-project)
+(setq ffip-patterns (append '("*.sass" "*.coffee") ffip-patterns))
+
+(require 'ruby-tools)
