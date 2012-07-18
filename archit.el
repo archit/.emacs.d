5d8517c3fbdb2f07cae9991f5d9781b0137c7fa4
Archit Baweja <archit@do.com>
2012-07-18 09:54:52 -0700

    Add some cuztomizations for key bindings and ruby mode.

--- archit.el
+++ archit.el
@@ -1,9 +1,28 @@
 (color-theme-twilight)
-(add-to-list 'auto-mode-alist
-             '("\\.js.coffee.erb$" . coffee-mode)
-             '("\\.*hbs.*$" . html-mode))
+(setq auto-mode-alist (add-to-list 'auto-mode-alist
+                                   '("\\.js.coffee.erb$" . coffee-mode)
+                                   '("\\.*hbs.*$" . html-mode)))
 
 (add-hook 'coffee-mode-hook '(lambda () (set (make-local-variable 'tab-width) 2)))
 
 (add-hook 'coffee-mode-hook
           '(lambda () (set (make-local-variable 'tab-width) 2)))
+
+(eval-after-load 'ruby-mode
+  '(progn
+     ;; stop the crazy indentation
+     (setq ruby-deep-indent-paren-style nil)
+
+     ;; always indent on newline
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)))
+
+(global-set-key (kbd "C-x C-f") 'find-file-other-window)
+(global-set-key (kbd "C-x C-t") 'ack-and-a-half)
+(global-set-key [f5] 'replace-string)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ack-and-a-half-executable "/usr/bin/ack-grep")
+ '(flymake-allowed-file-name-masks (quote (("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init) ("\\.xml\\'" flymake-xml-init) ("\\.html?\\'" flymake-xml-init) ("\\.cs\\'" flymake-simple-make-init) ("\\.p[ml]\\'" flymake-perl-init) ("\\.php[345]?\\'" flymake-php-init) ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup) ("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup) ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) ("\\.tex\\'" flymake-simple-tex-init) ("\\.idl\\'" flymake-simple-make-init)))))
