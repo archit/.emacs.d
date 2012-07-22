2948b5ca19dc35291701d85e2dc888a93be16e35
Archit Baweja <architbaweja@gmail.com>
2012-07-21 23:05:47 -0700

    Consolidate some settings.

--- archit.el
+++ archit.el
@@ -1,12 +1,20 @@
 (color-theme-twilight)
-(setq auto-mode-alist (add-to-list 'auto-mode-alist
-                                   '("\\.js.coffee.erb$" . coffee-mode)
-                                   '("\\.*hbs.*$" . html-mode)))
+(setq auto-mode-alist
+      (append '(("\\.js.coffee.erb$" . coffee-mode) ("\\..*hbs.*$" . html-mode))
+              auto-mode-alist))
 
-(add-hook 'coffee-mode-hook '(lambda () (set (make-local-variable 'tab-width) 2)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ack-and-a-half-executable (let ((ack-grep-bin (executable-find "ack-grep"))
+                                   (ack-bin (executable-find "ack")))
+                               (if ack-grep-bin ack-grep-bin
+                                 (if ack-bin ack-bin
+                                   nil)))))
 
-(add-hook 'coffee-mode-hook
-          '(lambda () (set (make-local-variable 'tab-width) 2)))
+(add-hook 'coffee-mode-hook '(lambda () (setq tab-width 2)))
 
 (eval-after-load 'ruby-mode
   '(progn
@@ -19,13 +27,9 @@
 (global-set-key (kbd "C-x C-f") 'find-file-other-window)
 (global-set-key (kbd "C-x C-t") 'ack-and-a-half)
 (global-set-key [f5] 'replace-string)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ack-and-a-half-executable "/usr/bin/ack-grep")
- '(flymake-allowed-file-name-masks (quote (("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init) ("\\.xml\\'" flymake-xml-init) ("\\.html?\\'" flymake-xml-init) ("\\.cs\\'" flymake-simple-make-init) ("\\.p[ml]\\'" flymake-perl-init) ("\\.php[345]?\\'" flymake-php-init) ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup) ("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup) ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) ("\\.tex\\'" flymake-simple-tex-init) ("\\.idl\\'" flymake-simple-make-init)))))
+
+;; make Cmd key act as Meta
+(setq ns-command-modifier 'meta)
 
 ; Find-file-in-project customizations
 (require 'find-file-in-project)
