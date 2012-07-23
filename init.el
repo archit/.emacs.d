eac3ada5b0145d324f0c1ffcb8f1bbb8cb5605d8
Archit Baweja <archit@do.com>
2012-07-23 12:57:34 -0700

    Default to archit.el for personal customization, no matter what the user name.

--- init.el
+++ init.el
@@ -67,7 +67,7 @@
 
 ;; You can keep system- or user-specific customizations here
 (setq system-specific-config (concat dotfiles-dir system-name ".el")
-      user-specific-config (concat dotfiles-dir user-login-name ".el")
+      user-specific-config (concat dotfiles-dir "archit" ".el")
       user-specific-dir (concat dotfiles-dir user-login-name))
 (add-to-list 'load-path user-specific-dir)
 
