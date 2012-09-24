f43519e1e4635bc502544db55fb17704495e80da
Archit Baweja <archit@do.com>
2012-09-24 15:57:06 -0700

    Added pry-me.

--- archit.el
+++ archit.el
@@ -9,17 +9,28 @@
 
 (add-hook 'coffee-mode-hook '(lambda () (setq tab-width 2)))
 
+; binding.pry my test/method/function in ruby
+(defun pry-me ()
+  (interactive)
+  (save-excursion
+    (insert "binding.pry\n")
+    (re-search-backward "should [\"'][^\"']*[\"'] do")
+    (forward-char 8)
+    (insert "pry ")))
+
 (eval-after-load 'ruby-mode
   '(progn
      ;; stop the crazy indentation
      (setq ruby-deep-indent-paren-style nil)
 
      ;; always indent on newline
-     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)))
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+
+     ;; add binding for pry-me
+     (define-key ruby-mode-map (kbd "C-x p") 'pry-me)))
 
 (global-set-key (kbd "C-x C-f") 'find-file-other-window)
 (global-set-key (kbd "C-x C-t") 'ack-and-a-half)
-(global-set-key [f5] 'replace-string)
 
 ;; make Cmd key act as Meta
 (setq ns-command-modifier 'meta)
@@ -30,5 +41,7 @@
 (setq ffip-patterns (append '("*.sass" "*.coffee") ffip-patterns))
 
 (require 'ruby-tools)
-
+(require 'rvm)
+(require 'package)
+(package-initialize)
 (server-start)
