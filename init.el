7c0e904d6cbd8e46e37882c6b810f782157339e4
Archit Baweja <architbaweja@gmail.com>
2019-07-15 13:15:11 +0530

    Setup rust-mode and friends.

--- init.el
+++ init.el
@@ -928,10 +928,21 @@ exec-to-string command, but it works and seems fast"
 
 ;;; starter-kit-js.el ends here
 
+;;; Rust customizations
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+(add-hook 'rust-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+(setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
+(setq racer-rust-src-path "~/Projects/rust/src") ;; Rust source code PATH
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
 ;;; Python Customizations
 (elpy-enable)
 (pyenv-mode)
-(load custom-file 'noerror)
 
 (defun pyvenv-autoload ()
   (require 'projectile)
